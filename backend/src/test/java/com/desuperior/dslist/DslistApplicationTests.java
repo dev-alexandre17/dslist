4f05db82dbb06adaac7c0e289f85bf3d753ea0a5
dev-alexandre17 <alexandretec77@gmail.com>
2023-05-11 10:15:21 -0300

    Project created

---
+++ backend/src/test/java/com/desuperior/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desuperior.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
